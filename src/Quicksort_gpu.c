53c09af7654a1564d2637a452e5e192362aac71f
alex <zuccheroalessio@gmail.com>
2024-10-11 19:08:54 +0200

    added realesekernel

--- src/Quicksort_gpu.c
+++ src/Quicksort_gpu.c
@@ -688,6 +688,7 @@ float* quickSortGpu(const float* vec,  const int nels, const int lws, const int 
 	clReleaseKernel(k.partitioning) ;
 	clReleaseKernel(k.scan_gpu) ;
 	clReleaseKernel(k.scan_update) ;
+	clReleaseKernel(k.quicksort_lmem) ; 
 
 	return out_copy ; 
 
